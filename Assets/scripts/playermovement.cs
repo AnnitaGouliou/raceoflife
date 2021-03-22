5cc322f97558e1e6dd6948b3bb55c50a3b8940aa
AnnitaGouliou <80201437+AnnitaGouliou@users.noreply.github.com>
2021-03-22 13:15:35 +0000

    Add files via upload

---
+++ Assets/scripts/playermovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+
+
+    private Rigidbody2D myRigidbody;
+
+    public float moveSpeed;
+    public float jumpForce;
+  
+
+  
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    
+
+
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+
+                {
+
+           
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+
+        }
+
+
+
+        
+
+
+    }
+
+
+
+}
